c2ff6c32f5772de5118a290da78a37858b37d534
Nick Ogden <nick@nickogden.org>
2018-09-30 17:36:53 +0100

    Rename sevice staring function aand add units to ttl for clarity

--- src/async_http/authentication.clj
+++ src/async_http/authentication.clj
@@ -21,9 +21,10 @@
   proto/Stoppable
   (proto/stop! [this] (async/close! comms)))
 
-(defn authentication-service
-  "A client that will request new secrets from `url` using `http-client`
-  every `ttl` milliseconds, and store them in `secret-store`.
+(defn start-service
+  "A client that will obtain new secrets by issuing `request` using
+  `http-client` every `ttl-ms` milliseconds, and store them in
+  `secret-store`.
 
   `opts` shall be passed to `http-client` when making each request and
   `response->secrets` shall be called on the response to extract the
@@ -33,39 +34,39 @@
 
     `:secrets` is required and contains the value that will be passed
     to `secret-store` for storage.
-    `ttl` is optional and, if provided, is the time to live for the new
-    secrets, otherwise the `ttl` value in `config` is used.
+    `ttl-ms` is optional and, if provided, is the time to live for the new
+    secrets, otherwise the `ttl-ms` value in `config` is used.
 
   The returned client shall begin to make requests immediatly upon creation
   and can be stopped by calling `stop!`"
   [http-client
    secret-store
-   {:keys [url opts response->secrets ttl] :as config}]
+   {:keys [request response->secrets ttl-ms] :as config}]
   (let [responses (async/chan 1)
         service   (AuthenticationService. responses)]
     (log/info "Starting authentication service client")
     (async/go-loop [timer (async/timeout 0)]
       (let [new-ttl
             (async/alt!
-              timer     ([_]
-                         (do (log/info "Requesting new secrets")
-                             (http/get http-client url opts
-                                       #(async/put! responses %))
-                             ttl))
-              responses ([response _]
+              timer     (do (log/info "Requesting new secrets")
+                            (http/request http-client
+                                          request
+                                          #(async/put! responses %))
+                            ttl-ms)
+              responses ([response]
                          (when response
                            (try
                              (let [{secrets :secrets
-                                    new-ttl :ttl
-                                    :or     {new-ttl ttl}}
+                                    new-ttl :ttl-ms
+                                    :or     {new-ttl ttl-ms}}
                                    (response->secrets response)]
                                (proto/update-secrets! secret-store secrets)
                                (log/info "New secrets received,"
                                          "next renewal in" new-ttl "ms")
                                new-ttl)
-                             (catch Throwable e
-                               (log/error e "Updating secrets failed")
-                               ttl)))))]
+                             (catch Exception e
+                               (log/error e "Updating secrets failed" (ex-data e))
+                               ttl-ms)))))]
         (if new-ttl
           (recur (async/timeout new-ttl))
           (log/info "Stopping authentication service client"))))
